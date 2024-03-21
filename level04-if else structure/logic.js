c64a844252b172690b5b99c033fc8f48b38abf1b
Marjan-ahmed <159646510+marjan-ahmed@users.noreply.github.com>
2024-03-21 15:16:21 +0500

    first commit

---
+++ level04-if else structure/logic.js
@@ -0,0 +1,17 @@
+"use strict";
+let totalMarks = 75;
+if (totalMarks >= 88) {
+    console.log("A+");
+}
+else if (totalMarks >= 75) {
+    console.log("A");
+}
+else if (totalMarks >= 60) {
+    console.log("B");
+}
+else if (totalMarks >= 55) {
+    console.log("C");
+}
+else if (totalMarks <= 40) {
+    console.log("Fail");
+}
---
+++ level04-if else structure/logic.ts
@@ -0,0 +1,26 @@
+let totalMarks : number = 75;
+
+if(totalMarks >= 88) 
+{
+    console.log("A+");
+}
+
+else if(totalMarks >= 75)
+{
+    console.log("A");
+}
+
+else if(totalMarks >= 60)
+{
+    console.log("B");
+}
+
+else if(totalMarks >= 55)
+{
+    console.log("C");
+}
+
+else if(totalMarks <= 40)
+{
+    console.log("Fail");
+}
