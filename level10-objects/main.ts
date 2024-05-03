7c110144dbcf2b649e1c80139e0366341a4f2c9e
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2024-05-03 21:02:01 +0500

    Add files via upload

---
+++ level10-objects/main.ts
@@ -0,0 +1,18 @@
+let aboutMe = {
+    fullName: "Marjan Ahmed",
+    age: 15,
+    gender: "male",
+    class: 9
+}
+console.log(aboutMe)
+console.log(`My name is ${aboutMe['fullName']} and I am ${aboutMe.age} years old`)
+
+let marks = {
+    79: "Islamiyat",
+    92: "chemistry",
+    88: "Physics",
+    97: "PST",
+    69: "maths",
+    73: "biology/computer science"
+}
+console.log(`My overall marks pf ISL, Chem, Physics are ${marks[79]} + ${marks[92]} + ${marks[88]} is ${79+92+88}`)
