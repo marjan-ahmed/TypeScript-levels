25c409aff0b15896ce91183b789621edb5f48cb7
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2024-05-03 21:01:27 +0500

    Initial Commit

---
+++ level09-functions/index.js
@@ -0,0 +1,14 @@
+"use strict";
+//Example 1:
+function greetings() {
+    const message = "Welcome Marjan";
+    return message;
+}
+;
+greetings();
+//Example 2:
+function sum(a = 2, b = 3) {
+    return a + b;
+}
+;
+sum();
---
+++ level09-functions/index.ts
@@ -0,0 +1,14 @@
+//Example 1:
+function greetings(){
+    const message: string = "Welcome Marjan";
+    return message;
+};
+
+greetings();
+
+//Example 2:
+function sum(a:number = 2, b:number = 3){
+    return a + b;
+};
+
+sum();
