41ad9c932d91cd0901d5f34adc386e7cc42f5284
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2024-07-19 13:33:06 +0300

    Add files via upload

---
+++ level13-array/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// String Array
+let fruits = ['mango', 'orange', 'apple'];
+//                         0       1        2
+console.log(fruits); // [ 'mango','orange','apple' ]
+// Find the length of an array
+console.log(fruits.length); // 3
+// Number Array
+let marks = [89, 73, 92];
+console.log(marks); // [ 89, 73, 92 ]
+// Find the length of an array
+console.log(marks.length); // 3
---
+++ level13-array/main.ts
@@ -0,0 +1,16 @@
+// String Array
+let fruits: string[] = ['mango','orange','apple'];
+//                         0       1        2
+console.log(fruits); // [ 'mango','orange','apple' ]
+
+// Find the length of an array
+console.log(fruits.length) // 3
+
+
+// Number Array
+let marks: number[] = [89, 73, 92];
+console.log(marks); // [ 89, 73, 92 ]
+
+// Find the length of an array
+console.log(marks.length) // 3
+ 
